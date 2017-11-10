3f4f3dced4156046d4abffb113c8ffccaf50fdee
SaraswathiRekhala <saraswathi.rekhala+11@wavemaker.com>
2017-11-10 11:02:35 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,14 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "vermillion",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "JavaServiceAutowiring",
+  "homePage" : "Main",
+  "name" : "JavaServiceAutowiring",
+  "platformType" : "WEB",
+  "securityEnabled" : "true",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
